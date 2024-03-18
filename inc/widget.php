4cf32e721a70fb4183bfa059d9d566c87c2cb3ab
Le V <4439012+lev777@users.noreply.github.com>
2024-03-18 09:59:39 -0500

    Update widget.php

--- inc/widget.php
+++ inc/widget.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Adding widget
+ */
+function Pm4wp_load_widget() {
+    register_widget( 'Pm4wp_Widget' );
+}
+add_action( 'widgets_init', 'Pm4wp_load_widget' );
 
 /**
  * PM4WP Widget Class
@@ -119,11 +126,5 @@ class Pm4wp_Widget extends WP_Widget
 	}
 }
 
-/**
- * Adding widget
- */
-function Pm4wp_load_widget() {
-    register_widget( 'Pm4wp_Widget' );
-}
-add_action( 'widgets_init', 'Pm4wp_load_widget' );
+
 
