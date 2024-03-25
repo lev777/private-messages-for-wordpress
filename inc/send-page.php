7daa0f96c35c41c889458cc527c1713d99e87cff
Le V <4439012+lev777@users.noreply.github.com>
2024-03-25 08:59:28 -0500

    Update send-page.php

--- inc/send-page.php
+++ inc/send-page.php
@@ -110,6 +110,27 @@ function rwpm_send()
 							$email_address = get_bloginfo( 'admin_email' );
 
 						$email_subject = strip_tags( $email_subject );
+
+						// Strip magic quotes from request data.
+						if (function_exists('get_magic_quotes_gpc') && get_magic_quotes_gpc()) {
+						    // Create lamba style unescaping function (for portability)
+						    $quotes_sybase = strtolower(ini_get('magic_quotes_sybase'));
+						    $unescape_function = (empty($quotes_sybase) || $quotes_sybase === 'off') ? 'stripslashes($value)' : 'str_replace("\'\'","\'",$value)';
+						    $stripslashes_deep = create_function('&$value, $fn', '
+						        if (is_string($value)) {
+						            $value = ' . $unescape_function . ';
+						        } else if (is_array($value)) {
+						            foreach ($value as &$v) $fn($v, $fn);
+						        }
+						    ');
+						    
+						    // Unescape data
+						    $stripslashes_deep($_POST, $stripslashes_deep);
+						    $stripslashes_deep($_GET, $stripslashes_deep);
+						    $stripslashes_deep($_COOKIE, $stripslashes_deep);
+						    $stripslashes_deep($_REQUEST, $stripslashes_deep);
+						}
+						
 						if ( get_magic_quotes_gpc() )
 						{
 							$email_subject = stripslashes( $email_subject );
